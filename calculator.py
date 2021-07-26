c8aaba8316f594c5ebd1da53e9e133f321a72bcf
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-27 07:47:38 +0800

    defined the window config

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""Calculator application"""
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("300x400")
+root.iconbitmap("calc.ico")
+root.resizable(0,0)
+
+# Run the window's main loop
+root.mainloop()
