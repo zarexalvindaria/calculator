51dd7641c56e1faed9899ad878aab79e7ed96c7b
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-27 07:53:39 +0800

    added the frames

--- calculator.py
+++ calculator.py
@@ -15,7 +15,18 @@ white_green = "#edefe0"
 button_font = ("Arial", 18)
 display_font = ("Arial", 30)
 
+# Define function
 
+# GUI layout
+# Define frames
+display_frame = tk.LabelFrame(root)
+button_frame = tk.LabelFrame(root)
+display_frame.pack()
+button_frame.pack()
+
+# Layout for the display frame
+display = tk.Entry(display_frame, width=50, font=display_font, bg=white_green, borderwidth=5)
+display.pack(padx=5, pady=5)
 
 # Run the window's main loop
 root.mainloop()
