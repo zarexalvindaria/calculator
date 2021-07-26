e23fb0afcfd9fc06d17c72c38e64526e4fae0d1f
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-27 07:49:58 +0800

    added the desired color scheme and typeface

--- calculator.py
+++ calculator.py
@@ -8,5 +8,14 @@ root.geometry("300x400")
 root.iconbitmap("calc.ico")
 root.resizable(0,0)
 
+# Define colors and fonts
+dark_green = "#93af22"
+light_green = "#acc253"
+white_green = "#edefe0"
+button_font = ("Arial", 18)
+display_font = ("Arial", 30)
+
+
+
 # Run the window's main loop
 root.mainloop()
